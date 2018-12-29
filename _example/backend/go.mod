7881f280a6774ef6574328973bdaeb31df0175bb
Umputun <umputun@gmail.com>
2018-12-28 23:10:01 -0600

    add data controller /private_data for #1

--- _example/backend/go.mod
+++ _example/backend/go.mod
@@ -2,8 +2,7 @@ module github.com/go-pkgz/auth/_example
 
 require (
 	github.com/go-chi/chi v3.3.3+incompatible
-	github.com/go-pkgz/auth v0.0.0-20181229022558-6952441be38f
+	github.com/go-pkgz/auth v0.0.0-20181229044404-3f248bf1fbe7
 	github.com/go-pkgz/rest v1.1.2
 	github.com/kr/pty v1.1.3 // indirect
-	golang.org/x/sys v0.0.0-20181228144115-9a3f9b0469bb // indirect
 )
--- _example/backend/main.go
+++ _example/backend/main.go
@@ -24,22 +24,24 @@ func main() {
 
 	/// define options
 	options := auth.Opts{
-		SecretReader:      token.SecretFunc(func(id string) (string, error) { return "secret", nil }), // secret key for JWT
-		TokenDuration:     time.Minute,
-		CookieDuration:    time.Hour * 24,
-		DisableXSRF:       true,
-		Issuer:            "my-demo-service",
-		URL:               "http://127.0.0.1:8080",
-		AvatarStore:       avatar.NewLocalFS("/tmp/demo-auth-service"),
-		AvatarResizeLimit: 200,
-		ClaimsUpd: token.ClaimsUpdFunc(func(claims token.Claims) token.Claims {
-			if claims.User != nil && claims.User.Name == "dev_admin" {
+		SecretReader: token.SecretFunc(func(id string) (string, error) { // secret key for JWT
+			return "secret", nil
+		}),
+		TokenDuration:     time.Minute,                                 // short token, refreshed automatically
+		CookieDuration:    time.Hour * 24,                              // cookie fine to keep for long time
+		DisableXSRF:       true,                                        // don't do this in real-life applications!
+		Issuer:            "my-demo-service",                           // part of token, just informational
+		URL:               "http://127.0.0.1:8080",                     // base url of the protected service
+		AvatarStore:       avatar.NewLocalFS("/tmp/demo-auth-service"), // stores avatars locally
+		AvatarResizeLimit: 200,                                         // resizes avatars to 200x20
+		ClaimsUpd: token.ClaimsUpdFunc(func(claims token.Claims) token.Claims { // modify issued token
+			if claims.User != nil && claims.User.Name == "dev_admin" { // set attributes for dev_admin
 				claims.User.SetAdmin(true)
 				claims.User.SetStrAttr("custom-key", "some value")
 			}
 			return claims
 		}),
-		Validator: token.ValidatorFunc(func(_ string, claims token.Claims) bool {
+		Validator: token.ValidatorFunc(func(_ string, claims token.Claims) bool { // rejects some tokens
 			return claims.User != nil && strings.HasPrefix(claims.User.Name, "dev_") // allow only dev_* names
 		}),
 	}
@@ -51,10 +53,11 @@ func main() {
 
 	// run dev/test oauth2 server on :8084
 	go func() {
-		p, err := service.Provider("dev")
+		p, err := service.Provider("dev") // peak dev provider
 		if err != nil {
 			log.Fatal(err)
 		}
+		// make and start dev auth server
 		devAuthServer := provider.DevAuthServer{Provider: p}
 		devAuthServer.Run()
 	}()
@@ -64,10 +67,14 @@ func main() {
 
 	// setup http server
 	router := chi.NewRouter()
-	router.Use(rest.AppInfo("auth-example", "umputun", "1.0.0"), rest.Ping)
-	router.Use(logger.Logger)
-	router.Get("/open", openRouteHandler)                      // open api
-	router.With(m.Auth).Get("/private", protectedRouteHandler) // protected api
+	router.Use(rest.AppInfo("auth-example", "umputun", "1.0.0"), rest.Ping) // add some external middlewares from go-pkgz/rest
+	router.Use(logger.Logger)                                               // log all http requests
+	router.Get("/open", openRouteHandler)                                   // open page
+	router.Group(func(r chi.Router) {
+		r.Use(m.Auth)
+		r.Get("/private", protectedPageHandler)      // protected page
+		r.Get("/private_data", protectedDataHandler) // protected api
+	})
 
 	// setup auth routes
 	authRoutes, avaRoutes := service.Handlers()
@@ -77,11 +84,13 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+// GET /open
 func openRouteHandler(w http.ResponseWriter, _ *http.Request) {
 	_, _ = w.Write([]byte("this is an open route, no token needed\n"))
 }
 
-func protectedRouteHandler(w http.ResponseWriter, r *http.Request) {
+// GET /private returns complete page for authenticated users only
+func protectedPageHandler(w http.ResponseWriter, r *http.Request) {
 	u := token.MustGetUserInfo(r)
 	b, err := json.MarshalIndent(u, "", "    ")
 	if err != nil {
@@ -89,6 +98,28 @@ func protectedRouteHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	p := fmt.Sprintf(page, string(b), u.Picture)
 	_, _ = w.Write([]byte(p))
+}
+
+// GET /private_data returns json with user info and ts
+func protectedDataHandler(w http.ResponseWriter, r *http.Request) {
+
+	u, err := token.GetUserInfo(r)
+	if err != nil {
+		rest.SendErrorJSON(w, r, http.StatusInternalServerError, err, "something wrong")
+		return
+	}
+
+	res := struct {
+		TS       time.Time `json:"ts"`
+		UserName string    `json:"user_name"`
+		Picture  string    `json:"picture"`
+	}{
+		TS:       time.Now(),
+		UserName: u.Name,
+		Picture:  u.Picture,
+	}
+
+	rest.RenderJSON(w, r, res)
 }
 
 var page = `
