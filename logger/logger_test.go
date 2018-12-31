b372bb47c73d1446faf77d80a34495fd1b8da710
Umputun <umputun@gmail.com>
2018-12-30 22:46:12 -0600

    test predefined loggers

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -3,13 +3,15 @@ package logger
 import (
 	"bytes"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLogger(t *testing.T) {
-
 	buff := bytes.NewBufferString("")
 	lg := Func(func(format string, args ...interface{}) {
 		fmt.Fprintf(buff, format, args...)
@@ -17,4 +19,22 @@ func TestLogger(t *testing.T) {
 
 	lg.Logf("blah %s %d something", "str", 123)
 	assert.Equal(t, "blah str 123 something", buff.String())
+}
+
+func TestStd(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	log.SetOutput(buff)
+	defer log.SetOutput(os.Stdout)
+
+	Std.Logf("blah %s %d something", "str", 123)
+	assert.True(t, strings.HasSuffix(buff.String(), "blah str 123 something\n"), buff.String())
+}
+
+func TestNoOp(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	log.SetOutput(buff)
+	defer log.SetOutput(os.Stdout)
+
+	NoOp.Logf("blah %s %d something", "str", 123)
+	assert.Equal(t, "", buff.String())
 }
