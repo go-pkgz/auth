b00e59437cda53c13032fdc22a28e109f628daa9
Umputun <umputun@gmail.com>
2019-07-02 17:32:55 -0500

    add smtp interface to allow more tests

--- provider/sender/email.go
+++ provider/sender/email.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/tls"
 	"fmt"
+	"io"
 	"net/smtp"
 
 	"github.com/pkg/errors"
@@ -15,7 +16,11 @@ import (
 // Uses common subject line and "from" for all messages
 type Email struct {
 	logger.L
+	SMTPClient
+	EmailParams
+}
 
+type EmailParams struct {
 	Host        string // SMTP host
 	Port        int    // SMTP port
 	From        string // From email field
@@ -27,70 +32,91 @@ type Email struct {
 	SMTPPassword string // password
 }
 
-// Send email with given text
-func (e *Email) Send(to string, text string) error {
+// SMTPClient interface defines subset of net/smtp used by email client
+type SMTPClient interface {
+	Mail(string) error
+	Auth(smtp.Auth) error
+	Rcpt(string) error
+	Data() (io.WriteCloser, error)
+	Quit() error
+	Close() error
+}
 
-	c, err := e.client()
-	if err != nil {
-		return errors.Wrap(err, "failed to make smtp client")
+// NewEmailClient creates email client with prepared smtp
+func NewEmailClient(p EmailParams, l logger.L) *Email {
+	return &Email{EmailParams: p, L: l, SMTPClient: nil}
+}
+
+// Send email with given text
+// If SMTPClient defined in Email struct uses it, if not - makes new smtp.Client on each send.
+// Always closes client on completion or failure.
+func (em *Email) Send(to string, text string) error {
+
+	client := em.SMTPClient
+	if client == nil { // if client not set make new net/smtp
+		c, err := em.client()
+		if err != nil {
+			return errors.Wrap(err, "failed to make smtp client")
+		}
+		client = c
 	}
 
 	var quit bool
 	defer func() {
-		if quit { // quit set after Quit() passed because it's closing connection too.
+		if quit { // quit set if Quit() call passed because it's closing connection as well.
 			return
 		}
-		if err = c.Close(); err != nil {
-			e.Logf("[WARN] can't close smtp connection, %v", err)
+		if err := client.Close(); err != nil {
+			em.Logf("[WARN] can't close smtp connection, %v", err)
 		}
 	}()
 
-	if e.SMTPUserName != "" && e.SMTPPassword != "" {
-		auth := smtp.PlainAuth("", e.SMTPUserName, e.SMTPPassword, e.Host)
-		if err = c.Auth(auth); err != nil {
-			return errors.Wrapf(err, "failed to auth to smtp %s:%d", e.Host, e.Port)
+	if em.SMTPUserName != "" && em.SMTPPassword != "" {
+		auth := smtp.PlainAuth("", em.SMTPUserName, em.SMTPPassword, em.Host)
+		if err := client.Auth(auth); err != nil {
+			return errors.Wrapf(err, "failed to auth to smtp %s:%d", em.Host, em.Port)
 		}
 	}
 
-	if err = c.Mail(e.From); err != nil {
-		return errors.Wrapf(err, "bad from address %q", e.From)
+	if err := client.Mail(em.From); err != nil {
+		return errors.Wrapf(err, "bad from address %q", em.From)
 	}
-	if err = c.Rcpt(to); err != nil {
+	if err := client.Rcpt(to); err != nil {
 		return errors.Wrapf(err, "bad to address %q", to)
 	}
 
-	writer, err := c.Data()
+	writer, err := client.Data()
 	if err != nil {
 		return errors.Wrap(err, "can't make email writer")
 	}
 
-	buf := bytes.NewBufferString(e.buildMessage(text, to))
+	buf := bytes.NewBufferString(em.buildMessage(text, to))
 	if _, err = buf.WriteTo(writer); err != nil {
 		return errors.Wrapf(err, "failed to send email body to %q", to)
 	}
 	if err = writer.Close(); err != nil {
-		e.Logf("[WARN] can't close smtp body writer, %v", err)
+		em.Logf("[WARN] can't close smtp body writer, %v", err)
 	}
 
-	if err = c.Quit(); err != nil {
-		e.Logf("[WARN] failed to send quit command to %s:%d, %v", e.Host, e.Port, err)
+	if err = client.Quit(); err != nil {
+		em.Logf("[WARN] failed to send quit command to %s:%d, %v", em.Host, em.Port, err)
 	}
 	quit = true
 	return nil
 }
 
-func (e *Email) client() (c *smtp.Client, err error) {
-	srvAddress := fmt.Sprintf("%s:%d", e.Host, e.Port)
-	if e.TLS {
+func (em *Email) client() (c *smtp.Client, err error) {
+	srvAddress := fmt.Sprintf("%s:%d", em.Host, em.Port)
+	if em.TLS {
 		tlsConf := &tls.Config{
 			InsecureSkipVerify: false,
-			ServerName:         e.Host,
+			ServerName:         em.Host,
 		}
 		conn, err := tls.Dial("tcp", srvAddress, tlsConf)
 		if err != nil {
 			return nil, errors.Wrapf(err, "failed to dial smtp tls to %s", srvAddress)
 		}
-		if c, err = smtp.NewClient(conn, e.Host); err != nil {
+		if c, err = smtp.NewClient(conn, em.Host); err != nil {
 			return nil, errors.Wrapf(err, "failed to make smtp client for %s", srvAddress)
 		}
 		return c, nil
@@ -103,12 +129,12 @@ func (e *Email) client() (c *smtp.Client, err error) {
 	return c, nil
 }
 
-func (e *Email) buildMessage(msg string, to string) (message string) {
-	message += fmt.Sprintf("From: %s\n", e.From)
+func (em *Email) buildMessage(msg string, to string) (message string) {
+	message += fmt.Sprintf("From: %s\n", em.From)
 	message += fmt.Sprintf("To: %s\n", to)
-	message += fmt.Sprintf("Subject: %s\n", e.Subject)
-	if e.ContentType != "" {
-		message += fmt.Sprintf("MIME-version: 1.0;\nContent-Type: %s; charset=\"UTF-8\";\n", e.ContentType)
+	message += fmt.Sprintf("Subject: %s\n", em.Subject)
+	if em.ContentType != "" {
+		message += fmt.Sprintf("MIME-version: 1.0;\nContent-Type: %s; charset=\"UTF-8\";\n", em.ContentType)
 	}
 	message += "\n" + msg
 	return message
--- provider/sender/email_test.go
+++ provider/sender/email_test.go
@@ -1,56 +1,121 @@
 package sender
 
 import (
+	"bytes"
+	"io"
+	"net/smtp"
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/go-pkgz/auth/logger"
 )
 
-// func TestEmailSend(t *testing.T) {
-// 	e := Email{
-// 		From:        "test@umputun.com",
-// 		L:           logger.Std,
-// 		ContentType: "text/html",
-// 		Host:        "192.168.1.24",
-// 		Port:        25,
-// 		Subject:     "test email",
-// 	}
-//
-// 	msg := `
-// <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-// <html>
-// <body>
-// <h2>rest</h2>
-// <pre>xyz</pre>
-// </body>
-// </html>
-// `
-// 	err := e.Send("sys@umputun.dev", msg)
-// 	assert.NoError(t, err)
-// }
+func TestEmailSend(t *testing.T) {
+	p := EmailParams{
+		From:        "test@umputun.com",
+		ContentType: "text/html",
+		Host:        "192.168.1.24",
+		Port:        25,
+		Subject:     "test email",
+	}
+	client := NewEmailClient(p, logger.Std)
+
+	msg := `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<body>
+<h2>rest</h2>
+<pre>xyz</pre>
+</body>
+</html>
+`
+	err := client.Send("sys@umputun.dev", msg)
+	assert.NoError(t, err)
+}
 
 func TestEmail_buildMessage(t *testing.T) {
-	e := Email{
-		From:    "from@example.com",
-		Subject: "subj",
-		L:       logger.Std,
-	}
+	p := EmailParams{From: "from@example.com", Subject: "subj"}
+	e := Email{L: logger.Std, EmailParams: p}
 
 	msg := e.buildMessage("this is a test\n12345", "to@example.com")
 	assert.Equal(t, "From: from@example.com\nTo: to@example.com\nSubject: subj\n\nthis is a test\n12345", msg)
 }
 
 func TestEmail_buildMessageWithMIME(t *testing.T) {
-	e := Email{
-		From:        "from@example.com",
-		L:           logger.Std,
-		ContentType: "text/html",
-		Subject:     "subj",
-	}
+
+	p := EmailParams{From: "from@example.com", Subject: "subj", ContentType: "text/html"}
+	e := Email{L: logger.Std, EmailParams: p}
 
 	msg := e.buildMessage("this is a test\n12345", "to@example.com")
 	assert.Equal(t, "From: from@example.com\nTo: to@example.com\nSubject: subj\nMIME-version: 1."+
 		"0;\nContent-Type: text/html; charset=\"UTF-8\";\n\nthis is a test\n12345", msg)
 }
+
+func TestEmail_New(t *testing.T) {
+	p := EmailParams{Host: "127.0.0.2", From: "from@example.com", Subject: "subj", ContentType: "text/html"}
+	e := NewEmailClient(p, logger.Std)
+	assert.Equal(t, p, e.EmailParams)
+}
+
+func TestEmail_Send(t *testing.T) {
+	fakeSmtp := &fakeTestSmtp{}
+	p := EmailParams{From: "from@example.com", Subject: "subj", ContentType: "text/html",
+		SMTPUserName: "user", SMTPPassword: "passwd"}
+	e := Email{L: logger.Std, EmailParams: p, SMTPClient: fakeSmtp}
+	err := e.Send("to@example.com", "some text")
+	require.NoError(t, err)
+
+	assert.Equal(t, "from@example.com", fakeSmtp.mail)
+	assert.Equal(t, "to@example.com", fakeSmtp.rcpt)
+	assert.Equal(t, "From: from@example.com\nTo: to@example.com\nSubject: subj\n"+
+		"MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\nsome text", fakeSmtp.buff.String())
+	assert.True(t, fakeSmtp.auth)
+	assert.True(t, fakeSmtp.quit)
+	assert.False(t, fakeSmtp.close)
+}
+
+func TestEmail_SendFailed(t *testing.T) {
+	fakeSmtp := &fakeTestSmtp{fail: true}
+	p := EmailParams{From: "from@example.com", Subject: "subj", ContentType: "text/html"}
+	e := Email{L: logger.Std, EmailParams: p, SMTPClient: fakeSmtp}
+	err := e.Send("to@example.com", "some text")
+	require.EqualError(t, err, "can't make email writer: failed")
+
+	assert.Equal(t, "from@example.com", fakeSmtp.mail)
+	assert.Equal(t, "to@example.com", fakeSmtp.rcpt)
+	assert.Equal(t, "", fakeSmtp.buff.String())
+	assert.False(t, fakeSmtp.auth)
+	assert.False(t, fakeSmtp.quit)
+	assert.True(t, fakeSmtp.close)
+}
+
+type fakeTestSmtp struct {
+	fail bool
+
+	buff        bytes.Buffer
+	mail, rcpt  string
+	auth        bool
+	quit, close bool
+}
+
+func (f *fakeTestSmtp) Mail(m string) error  { f.mail = m; return nil }
+func (f *fakeTestSmtp) Auth(smtp.Auth) error { f.auth = true; return nil }
+func (f *fakeTestSmtp) Rcpt(r string) error  { f.rcpt = r; return nil }
+func (f *fakeTestSmtp) Quit() error          { f.quit = true; return nil }
+func (f *fakeTestSmtp) Close() error         { f.close = true; return nil }
+
+func (f *fakeTestSmtp) Data() (io.WriteCloser, error) {
+	if f.fail {
+		return nil, errors.New("failed")
+	}
+	return nopCloser{&f.buff}, nil
+}
+
+type nopCloser struct {
+	io.Writer
+}
+
+func (nopCloser) Close() error { return nil }
