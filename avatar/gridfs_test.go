886247fbe42337419b330629c85334347dc96ef6
Umputun <umputun@gmail.com>
2019-10-02 23:02:52 -0500

    Switch to official mongo driver (#36)
    * migration to the official mongo driver
    
    * add injected timeout to gridfs store
    
    * lint: minor warnings
    
    * switch to go 1.13 and latest linter

--- avatar/gridfs_test.go
+++ avatar/gridfs_test.go
@@ -1,23 +1,21 @@
 package avatar
 
 import (
+	"context"
 	"io/ioutil"
 	"sort"
 	"strings"
 	"testing"
-
-	"github.com/globalsign/mgo"
-	"github.com/go-pkgz/mongo"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func TestGridFS_PutAndGet(t *testing.T) {
-	p, skip := prepGFStore(t)
-	if skip {
-		return
-	}
+	p := prepGFStore(t)
 	avatar, err := p.Put("user1", strings.NewReader("some picture bin data"))
 	require.Nil(t, err)
 	assert.Equal(t, "b3daa77b4c04a9551b8781d03191fe098f325e67.image", avatar)
@@ -31,8 +29,7 @@ func TestGridFS_PutAndGet(t *testing.T) {
 
 	_, _, err = p.Get("bad avatar")
 	assert.NotNil(t, err)
-
-	assert.Equal(t, "8ce5568f7f9a1c9da5b897bc8642e397", p.ID(avatar))
+	assert.Equal(t, "fddae9ce556712a6ece0e8951a6e7a05c51ed6bf", p.ID(avatar))
 	assert.Equal(t, "70c881d4a26984ddce795f6f71817c9cf4480e79", p.ID("aaaa"), "no data, encode avatar id")
 
 	l, err := p.List()
@@ -42,13 +39,8 @@ func TestGridFS_PutAndGet(t *testing.T) {
 }
 
 func TestGridFS_Remove(t *testing.T) {
-	p, skip := prepGFStore(t)
-	if skip {
-		return
-	}
-
+	p := prepGFStore(t)
 	assert.NotNil(t, p.Remove("no-such-thing.image"))
-
 	avatar, err := p.Put("user1", strings.NewReader("some picture bin data"))
 	require.Nil(t, err)
 	assert.Equal(t, "b3daa77b4c04a9551b8781d03191fe098f325e67.image", avatar)
@@ -57,10 +49,8 @@ func TestGridFS_Remove(t *testing.T) {
 }
 
 func TestGridFS_List(t *testing.T) {
-	p, skip := prepGFStore(t)
-	if skip {
-		return
-	}
+	p := prepGFStore(t)
+
 	// write some avatars
 	_, err := p.Put("user1", strings.NewReader("some picture bin data 1"))
 	require.Nil(t, err)
@@ -83,16 +73,16 @@ func TestGridFS_List(t *testing.T) {
 	assert.Equal(t, "some picture bin data 3", string(data))
 }
 
-func prepGFStore(t *testing.T) (Store, bool) {
-	conn, err := mongo.MakeTestConnection(t)
-	if err != nil {
-		return nil, true
-	}
-	_ = conn.WithCustomCollection("fs.chunks", func(coll *mgo.Collection) error {
-		return coll.DropCollection()
-	})
-	_ = conn.WithCustomCollection("fs.files", func(coll *mgo.Collection) error {
-		return coll.DropCollection()
-	})
-	return NewGridFS(conn), false
+func prepGFStore(t *testing.T) *GridFS {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	require.NoError(t, err)
+
+	_ = client.Database("test").Collection("ava_fs.chunks").Drop(ctx)
+	_ = client.Database("test").Collection("ava_fs.files").Drop(ctx)
+
+	return NewGridFS(client, "test", "ava_fs", time.Second)
 }
