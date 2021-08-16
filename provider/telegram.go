fed26c31c35ce67e9a960490b6107249c602f82d
Dmitry Verkhoturov <paskal.07@gmail.com>
2021-08-16 20:29:03 +0200

    add Audience, ExpiresAt and NotBefore to telegram JWT token

--- provider/telegram.go
+++ provider/telegram.go
@@ -242,8 +242,11 @@ func (th *TelegramHandler) LoginHandler(w http.ResponseWriter, r *http.Request) 
 	claims := authtoken.Claims{
 		User: &u,
 		StandardClaims: jwt.StandardClaims{
-			Id:     queryToken,
-			Issuer: th.ProviderName,
+			Audience:  r.URL.Query().Get("site"),
+			Id:        queryToken,
+			Issuer:    th.ProviderName,
+			ExpiresAt: time.Now().Add(30 * time.Minute).Unix(),
+			NotBefore: time.Now().Add(-1 * time.Minute).Unix(),
 		},
 		SessionOnly: false, // TODO
 	}
@@ -261,11 +264,11 @@ func (th *TelegramHandler) LoginHandler(w http.ResponseWriter, r *http.Request) 
 	delete(th.requests.data, queryToken)
 }
 
-// AuthHandler does nothing since we're don't have any callbacks
-func (th *TelegramHandler) AuthHandler(w http.ResponseWriter, r *http.Request) {}
+// AuthHandler does nothing since we don't have any callbacks
+func (th *TelegramHandler) AuthHandler(_ http.ResponseWriter, _ *http.Request) {}
 
 // LogoutHandler - GET /logout
-func (th *TelegramHandler) LogoutHandler(w http.ResponseWriter, r *http.Request) {
+func (th *TelegramHandler) LogoutHandler(w http.ResponseWriter, _ *http.Request) {
 	th.TokenService.Reset(w)
 }
 
