92295edb8531630016c41d11d6fb115ab24518f8
Dmitry Verkhoturov <paskal.07@gmail.com>
2022-10-02 23:38:39 +0200

    bump go modules,switch from local email client to go-pkgz/email
    The local client started lagging behind go-pkgz/email feature-wise.

--- go.mod
+++ go.mod
@@ -4,15 +4,16 @@ go 1.17
 
 require (
 	github.com/dghubble/oauth1 v0.7.1
-	github.com/go-oauth2/oauth2/v4 v4.4.3
+	github.com/go-oauth2/oauth2/v4 v4.5.1
+	github.com/go-pkgz/email v0.4.0
 	github.com/go-pkgz/repeater v1.1.3
-	github.com/go-pkgz/rest v1.14.0
+	github.com/go-pkgz/rest v1.16.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/microcosm-cc/bluemonday v1.0.19
+	github.com/microcosm-cc/bluemonday v1.0.20
 	github.com/nullrocks/identicon v0.0.0-20180626043057-7875f45b0022
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.8.0
 	go.etcd.io/bbolt v1.3.6
-	go.mongodb.org/mongo-driver v1.8.5
+	go.mongodb.org/mongo-driver v1.10.2
 	golang.org/x/image v0.0.0-20220413100746-70e8d0d3baa9
 	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
 )
@@ -21,12 +22,13 @@ require (
 	cloud.google.com/go/compute v1.6.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/uuid v1.1.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/klauspost/compress v1.15.2 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/tidwall/btree v0.0.0-20191029221954-400434d76274 // indirect
@@ -41,12 +43,12 @@ require (
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
-	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
-	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- provider/sender/email_test.go
+++ provider/sender/email_test.go
@@ -1,18 +1,13 @@
 package sender
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"net/smtp"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/go-pkgz/auth/logger"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"github.com/go-pkgz/auth/logger"
 )
 
 func TestEmailSend(t *testing.T) {
@@ -41,74 +36,24 @@ func TestEmailSend(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestEmail_buildMessage(t *testing.T) {
-	p := EmailParams{From: "from@example.com", Subject: "subj"}
-	e := Email{L: logger.Std, EmailParams: p}
-
-	msg, err := e.buildMessage("this is a test\n12345\n", "to@example.com")
-	require.NoError(t, err)
-	assert.Contains(t, msg, "From: from@example.com\nTo: to@example.com\nSubject: subj\n", msg)
-	assert.Contains(t, msg, "this is a test\r\n12345", msg)
-	assert.Contains(t, msg, "Date: ", msg)
-	assert.Contains(t, msg, "Content-Transfer-Encoding: quoted-printable", msg)
-}
-
-func TestEmail_buildMessageWithMIME(t *testing.T) {
-
-	p := EmailParams{From: "from@example.com", Subject: "subj", ContentType: "text/html"}
-	e := Email{L: logger.Std, EmailParams: p}
-
-	msg, err := e.buildMessage("this is a test\n12345\n", "to@example.com")
-	require.NoError(t, err)
-	assert.Contains(t, msg, "From: from@example.com\nTo: to@example.com\nSubject: subj\nContent-Transfer-Encoding: quoted-printable\nMIME-version: 1.0\nContent-Type: text/html; charset=\"UTF-8\"", msg)
-	assert.Contains(t, msg, "\n\nthis is a test\r\n12345", msg)
-	assert.Contains(t, msg, "Date: ", msg)
-}
-
 func TestEmail_New(t *testing.T) {
-	p := EmailParams{Host: "127.0.0.2", From: "from@example.com", Subject: "subj", ContentType: "text/html"}
+	p := EmailParams{
+		Host:         "127.0.0.2",
+		From:         "from@example.com",
+		SMTPUserName: "user",
+		SMTPPassword: "pass",
+		Subject:      "subj",
+		ContentType:  "text/html",
+		Charset:      "UTF-8",
+		LoginAuth:    true,
+		StartTLS:     true,
+		TLS:          true,
+	}
 	e := NewEmailClient(p, logger.Std)
 	assert.Equal(t, p, e.EmailParams)
 }
 
-func TestEmail_Send(t *testing.T) {
-	fakeSMTP := &fakeTestSMTP{}
-	p := EmailParams{From: "from@example.com", Subject: "subj", ContentType: "text/html",
-		SMTPUserName: "user", SMTPPassword: "passwd"}
-	e := Email{L: logger.Std, EmailParams: p, SMTPClient: fakeSMTP}
-	err := e.Send("to@example.com", "some text\n")
-	require.NoError(t, err)
-
-	assert.Equal(t, "from@example.com", fakeSMTP.mail)
-	assert.Equal(t, "to@example.com", fakeSMTP.rcpt)
-	msg := fakeSMTP.buff.String()
-	assert.Contains(t, msg, "From: from@example.com\nTo: to@example."+
-		"com\nSubject: subj\nContent-Transfer-Encoding: quoted-printable\nMIME-version: 1."+
-		"0\nContent-Type: text/html; charset=\"UTF-8\"", msg)
-	assert.Contains(t, msg, "\n\nsome text", msg)
-	assert.Contains(t, msg, "Date: ", msg)
-
-	assert.True(t, fakeSMTP.auth)
-	assert.True(t, fakeSMTP.quit)
-	assert.False(t, fakeSMTP.close)
-}
-
 func TestEmail_SendFailed(t *testing.T) {
-	fakeSMTP := &fakeTestSMTP{fail: true}
-	p := EmailParams{From: "from@example.com", Subject: "subj", ContentType: "text/html"}
-	e := Email{L: logger.Std, EmailParams: p, SMTPClient: fakeSMTP}
-	err := e.Send("to@example.com", "some text")
-	require.EqualError(t, err, "can't make email writer: failed")
-
-	assert.Equal(t, "from@example.com", fakeSMTP.mail)
-	assert.Equal(t, "to@example.com", fakeSMTP.rcpt)
-	assert.Equal(t, "", fakeSMTP.buff.String())
-	assert.False(t, fakeSMTP.auth)
-	assert.False(t, fakeSMTP.quit)
-	assert.True(t, fakeSMTP.close)
-}
-
-func TestEmail_SendFailed2(t *testing.T) {
 	p := EmailParams{Host: "127.0.0.2", Port: 25, From: "from@example.com",
 		Subject: "subj", ContentType: "text/html", TimeOut: time.Millisecond * 200}
 	e := NewEmailClient(p, logger.Std)
@@ -122,31 +67,3 @@ func TestEmail_SendFailed2(t *testing.T) {
 	err = e.Send("to@example.com", "some text")
 	require.NotNil(t, err)
 }
-
-type fakeTestSMTP struct {
-	fail bool
-
-	buff        bytes.Buffer
-	mail, rcpt  string
-	auth        bool
-	quit, close bool
-}
-
-func (f *fakeTestSMTP) Mail(m string) error  { f.mail = m; return nil }
-func (f *fakeTestSMTP) Auth(smtp.Auth) error { f.auth = true; return nil }
-func (f *fakeTestSMTP) Rcpt(r string) error  { f.rcpt = r; return nil }
-func (f *fakeTestSMTP) Quit() error          { f.quit = true; return nil }
-func (f *fakeTestSMTP) Close() error         { f.close = true; return nil }
-
-func (f *fakeTestSMTP) Data() (io.WriteCloser, error) {
-	if f.fail {
-		return nil, fmt.Errorf("failed")
-	}
-	return nopCloser{&f.buff}, nil
-}
-
-type nopCloser struct {
-	io.Writer
-}
-
-func (nopCloser) Close() error { return nil }
